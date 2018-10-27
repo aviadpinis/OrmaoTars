cc8e0b75c2dfc91b798877ed99fb0e5781c1d868
Unknown <aviadpinis@gmail.com>
2018-10-27 23:58:41 +0300

    ReadMe catsVsDogs

--- CatsVsDogs/test_cats_vs_dogs.py
+++ CatsVsDogs/test_cats_vs_dogs.py
@@ -6,7 +6,7 @@ from keras.preprocessing.image  import ImageDataGenerator
 test_path = './test/'
 IMAGE_SIZE = 224
 
-test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(IMAGE_SIZE,IMAGE_SIZE), classes=['cat','dog'], batch_size=200)
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(IMAGE_SIZE,IMAGE_SIZE), classes=['cat','dog'], batch_size=20)
 
 model = Sequential()
 model.load("model.h5")
